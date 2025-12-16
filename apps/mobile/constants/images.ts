99f09d09b8c86e28a3f104fe9edda512c80044c9
aurickwilliam <aurickwilliam21@gmail.com>
2025-12-16 22:28:40 +0800

    DONE: Tenant Sign In

--- apps/mobile/constants/images.ts
+++ apps/mobile/constants/images.ts
@@ -1,3 +1,5 @@
 export const IMAGES = {
   logo: require("@/assets/images/logo.png"),
+  googleLogo: require("@/assets/images/third-party-logos/google.png"),
+  facebookLogo: require("@/assets/images/third-party-logos/facebook.png"),
 };
