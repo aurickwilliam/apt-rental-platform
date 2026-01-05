95854825cd79ac0a27c230b63aac1febca81d6c1
aurickwilliam <aurickwilliam21@gmail.com>
2026-01-05 22:40:33 +0800

    ADDED: Bottom Sheet Dropdown

--- apps/mobile/app/(auth)/_layout.tsx
+++ apps/mobile/app/(auth)/_layout.tsx
@@ -1,68 +1,74 @@
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function _Layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen
-        name="onboarding"
-        options={{
-          title: "Onboarding",
-          headerShown: false,
-        }}
-      />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen
+            name="onboarding"
+            options={{
+              title: "Onboarding",
+              headerShown: false,
+            }}
+          />
 
-      <Stack.Screen
-        name="sign-in"
-        options={{
-          title: "Sign In",
-          headerShown: false,
-        }}
-      />
+          <Stack.Screen
+            name="sign-in"
+            options={{
+              title: "Sign In",
+              headerShown: false,
+            }}
+          />
 
-      <Stack.Screen
-        name="sign-up"
-        options={{
-          title: "Sign Up",
-          headerShown: false,
-        }}
-      />
+          <Stack.Screen
+            name="sign-up"
+            options={{
+              title: "Sign Up",
+              headerShown: false,
+            }}
+          />
 
-      <Stack.Screen
-        name="forgot-password"
-        options={{
-          title: "Forgot Password",
-          headerShown: false,
-        }}
-      />
+          <Stack.Screen
+            name="forgot-password"
+            options={{
+              title: "Forgot Password",
+              headerShown: false,
+            }}
+          />
 
-      <Stack.Screen
-        name="complete-profile"
-        options={{
-          title: "Complete Profile",
-          headerShown: false,
-        }}
-      />
+          <Stack.Screen
+            name="complete-profile"
+            options={{
+              title: "Complete Profile",
+              headerShown: false,
+            }}
+          />
 
-      <Stack.Screen
-        name="verify-mobile"
-        options={{
-          title: "Verify Email",
-          headerShown: false,
-        }}
-      />
+          <Stack.Screen
+            name="verify-mobile"
+            options={{
+              title: "Verify Email",
+              headerShown: false,
+            }}
+          />
 
-      <Stack.Screen
-        name="otp-verification"
-        options={{
-          title: "OTP Verification",
-          headerShown: false,
-        }}
-      />
+          <Stack.Screen
+            name="otp-verification"
+            options={{
+              title: "OTP Verification",
+              headerShown: false,
+            }}
+          />
 
-    </Stack>
+        </Stack>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 }
--- apps/mobile/components/DropdownField.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-
-export default function DropdownField() {
-  return (
-    <View>
-      <Text>DropdownField</Text>
-    </View>
-  )
-}
