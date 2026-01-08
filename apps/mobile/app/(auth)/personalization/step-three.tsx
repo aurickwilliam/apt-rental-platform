23e81495cfc0df93497f22c88069422165db85bd
aurickwilliam <aurickwilliam21@gmail.com>
2026-01-08 17:47:33 +0800

    DONE: Step Two and Three

--- apps/mobile/app/(auth)/personalization/step-three.tsx
+++ apps/mobile/app/(auth)/personalization/step-three.tsx
@@ -1,10 +1,90 @@
+import { Text, Pressable, View } from "react-native";
+import { useRouter } from 'expo-router';
+
 import ScreenWrapper from "../../../components/ScreenWrapper";
-import { Text } from "react-native";
+import PillButton from "@/components/PillButton";
+import { useState } from "react";
+import BedroomRadioButton from "@/components/BedroomRadioButton";
 
 export default function StepThree() {
+  const router = useRouter();
+
+  type bedroomType = {
+    bedRoomLabel: string;
+    isSelected: boolean;
+  }
+
+  const [selectedBedroomCount, setSelectedBedroomCount] = useState<bedroomType[]>([
+    { bedRoomLabel: "1-2 Bedrooms", isSelected: false },
+    { bedRoomLabel: "2-4 Bedrooms", isSelected: false },
+    { bedRoomLabel: "4+ Bedrooms", isSelected: false },
+  ]);
+
+  const toggleBedroomByIndex = (index: number) => {
+    setSelectedBedroomCount(prev =>
+      prev.map((bedroom, i) => ({
+        ...bedroom,
+        isSelected: i === index
+      }))
+    );
+  };
+
+  const handleSkip = () => {
+    // router.replace("/personalization/step-three");
+    // TODO: Navigate to the tabs page
+  };
+
+  const handleNext = () => {
+    // router.replace("/personalization/step-three");
+    // TODO: Navigate to the tabs page
+  };
+
   return (
-    <ScreenWrapper className="px-5 pt-5">
-      <Text>Step Three</Text>
+    <ScreenWrapper>
+      {/* Skip Button*/}
+      <Pressable onPress={handleSkip}>
+        <Text className="text-grey-300 text-base font-inter">
+          Skip
+        </Text>
+      </Pressable>
+
+      {/* Question and Descriptio */}
+      <View className="flex gap-3 my-5">
+        {/* Question */}
+        <Text className="text-secondary text-3xl font-dmserif">
+          How many bedrooms are you looking for?
+        </Text>
+
+        {/* Description */}
+        <Text className="text-text text-lg font-inter">
+          Select the number of bedrooms you prefer so we can filter properties 
+          that fit your needs.
+        </Text>
+      </View>
+
+      {/* Bedroom Radio Buttons */}
+      <View className="flex-row flex-wrap justify-between">
+        {
+          selectedBedroomCount.map((bedroom, index) => (
+            <BedroomRadioButton
+              key={index}
+              bedRoomLabel={bedroom.bedRoomLabel}
+              selected={bedroom.isSelected}
+              onPress={() => toggleBedroomByIndex(index)}
+            />
+          ))
+        }
+      </View>
+
+      {/* Spacer */}
+      <View className="flex-1" />
+
+      {/* Next Button*/}
+      <PillButton
+        label={"Let's find your place!"}
+        onPress={handleNext}
+        isFullWidth
+      />
     </ScreenWrapper>
   );
 }
--- apps/mobile/app/(auth)/personalization/step-two.tsx
+++ apps/mobile/app/(auth)/personalization/step-two.tsx
@@ -1,10 +1,84 @@
+import { Text, Pressable, View } from "react-native";
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import { Slider } from '@miblanchard/react-native-slider';
+
+import { COLORS } from "../../../constants/colors";
+
 import ScreenWrapper from "../../../components/ScreenWrapper";
-import { Text } from "react-native";
+import PillButton from "../../../components/PillButton";
 
 export default function StepTwo() {
+  const router = useRouter();
+
+  const [rentAmount, setRentAmount] = useState<number>(10_000);
+
+  const handleSkip = () => {
+    router.replace("/personalization/step-three");
+  };
+
+  const handleNext = () => {
+    router.replace("/personalization/step-three");
+  };
+
   return (
-    <ScreenWrapper className="px-5 pt-5">
-      <Text>Step Two</Text>
+    <ScreenWrapper>
+      {/* Skip Button*/}
+      <Pressable onPress={handleSkip}>
+        <Text className="text-grey-300 text-base font-inter">
+          Skip
+        </Text>
+      </Pressable>
+
+      {/* Question and Descriptio */}
+      <View className="flex gap-3 my-5">
+        {/* Question */}
+        <Text className="text-secondary text-3xl font-dmserif">
+          Got a budget in mind for your place?
+        </Text>
+
+        {/* Description */}
+        <Text className="text-text text-lg font-inter">
+          Select your preferred price range to see listings that fit your budget.
+        </Text>
+      </View>
+      
+      <View className="flex-1 items-center justify-center gap-10">
+        {/* Rent Amount */}
+        <Text className="text-primary text-6xl font-dmserif">
+          ₱ {rentAmount.toLocaleString()}
+        </Text>
+
+        {/* Slider */}
+        <Slider
+          value={rentAmount}
+          onValueChange={(value) => setRentAmount(value[0])}
+          minimumValue={2_000}
+          maximumValue={50_000}
+          step={1_000}
+          minimumTrackTintColor={ COLORS.primary }
+          thumbTintColor={COLORS.primary}
+          thumbStyle={{
+            width: 20,
+            height: 20,
+            borderRadius: 100
+          }}
+          trackStyle={{
+            height: 10,
+            borderRadius: 50,
+            backgroundColor: COLORS.lightLightLightGrey
+          }}
+          containerStyle={{ width: '90%' }}
+        />
+      </View>
+
+      {/* Next Button*/}
+      <PillButton
+        label={"Next"}
+        onPress={handleNext}
+        isFullWidth
+      />
+
     </ScreenWrapper>
   );
 }
