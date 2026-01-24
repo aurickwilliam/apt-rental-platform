80a196ea5862cc9f2206bd741d14dfb5672ecca8
aurickwilliam <aurickwilliam21@gmail.com>
2026-01-25 00:16:32 +0800

    DONE: Profile Images and Verification Status

--- apps/mobile/constants/onboarding-data.ts
+++ apps/mobile/constants/onboarding-data.ts
@@ -1,4 +1,4 @@
-import { IMAGES } from "../constants/images";
+import { ONBOARDING_IMAGES } from "../constants/images";
 import { ImageSourcePropType } from "react-native";
 
 interface Slide {
@@ -13,24 +13,24 @@ export const SLIDES: Slide[] = [
     id: 1,
     title: 'Welcome!',
     description: 'Find your perfect apartment in just a few taps.',
-    imagePath: IMAGES.onboardingImage1,
+    imagePath: ONBOARDING_IMAGES.onboardingImage1,
   },
   {
     id: 2,
     title: 'Explore Rentals',
     description: 'Browse apartments by location, price, and amenities.',
-    imagePath: IMAGES.onboardingImage2,
+    imagePath: ONBOARDING_IMAGES.onboardingImage2,
   },
   {
     id: 3,
     title: 'Secure & Easy Transactions',
     description: 'Pay rent securely and chat directly with landlords.',
-    imagePath: IMAGES.onboardingImage3,
+    imagePath: ONBOARDING_IMAGES.onboardingImage3,
   },
   {
     id: 4,
     title: 'Verified Listings',
     description: 'All listings are verified to ensure trust and safety.',
-    imagePath: IMAGES.onboardingImage4,
+    imagePath: ONBOARDING_IMAGES.onboardingImage4,
   },
 ];
