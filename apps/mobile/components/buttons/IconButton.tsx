416756467939d1121ea1ac28ef8bc6fbad17de8e
aurickwilliam <aurickwilliam21@gmail.com>
2026-01-27 21:46:07 +0800

    DONE: Carousel Image, Buttons, and Apartment Details

---
+++ apps/mobile/components/buttons/IconButton.tsx
@@ -0,0 +1,32 @@
+import { TouchableOpacity } from 'react-native';
+import { IconProps } from '@tabler/icons-react-native';
+
+import { COLORS } from '../../constants/colors';
+
+interface IconButtonProps {
+  iconName: React.ComponentType<IconProps>;
+  buttonColor?: string;
+  iconColor?: string;
+}
+
+export default function IconButton({
+  iconName: IconName,
+  buttonColor = COLORS.white,
+  iconColor = COLORS.text,
+}: IconButtonProps) {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className={`p-4 rounded-full`}
+      style={{
+        backgroundColor: buttonColor,
+        opacity: 0.9
+      }}
+    >
+      <IconName
+        size={24}
+        color={iconColor}
+      />
+    </TouchableOpacity>
+  );
+}
