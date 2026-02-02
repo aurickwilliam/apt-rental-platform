895dbe3f5ed89b9612763722d1eb6e21f7632c49
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-02 09:34:43 +0800

    DONE: Review Page

---
+++ apps/mobile/app/apartment/[apartmentId]/_layout.tsx
@@ -0,0 +1,24 @@
+import { Stack } from "expo-router"
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
+export default function ApartmentLayout() {
+  return (
+    <GestureHandlerRootView style={{flex: 1}}>
+      <BottomSheetModalProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen 
+            name="ratings"
+          />
+          <Stack.Screen 
+            name="rate-apartment"
+          />
+        </Stack>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+  )
+}
