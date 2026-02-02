f01096a3586b8b4c2d8167c4d8b8c27d3ec4fb78
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-02 09:34:43 +0800

    DONE: Review Page

--- apps/mobile/app/apartment/[apartmentId]/ratings.tsx
+++ apps/mobile/app/apartment/[apartmentId]/ratings.tsx
@@ -1,19 +1,28 @@
 import { View, Text } from 'react-native'
-import { useLocalSearchParams } from 'expo-router'
+import { useState } from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 
 import ScreenWrapper from 'components/layout/ScreenWrapper'
 import StandardHeader from 'components/layout/StandardHeader'
+import RatingBarCount from 'components/display/RatingBarCount'
+import DropdownButton from 'components/buttons/DropdownButton'
+import Divider from 'components/display/Divider'
+import RatingCard from 'components/display/RatingCard'
+import PillButton from 'components/buttons/PillButton'
 
 import { COLORS } from '../../../constants/colors'
 
 import {
   IconStarFilled,
   IconStar,
+  IconCirclePlus,
 } from '@tabler/icons-react-native';
-import RatingBarCount from 'components/display/RatingBarCount'
 
 export default function RatingsPage() {
   const { apartmentId } = useLocalSearchParams<{ apartmentId: string }>();
+  const router = useRouter();
+
+  const [selectedFilter, setSelectedFilter] = useState<'Most Recent' | 'Highest Rating' | 'Lowest Rating'>('Most Recent');
 
   // Dummy data for demonstration purposes
   const ratings = {
@@ -25,8 +34,39 @@ export default function RatingsPage() {
       { rating: 3, ratingCount: 10, totalCount: 120 },
       { rating: 2, ratingCount: 3, totalCount: 120 },
       { rating: 1, ratingCount: 2, totalCount: 120 },
+    ],
+    reviews: [
+      {
+        id: 1,
+        name: 'John Doe',
+        date: '2023-08-15',
+        rating: 5.0,
+        review: 'Great place to stay!',
+        durationOfStay: 'Jan 2023 - Mar 2023',
+      },
+      {
+        id: 2,
+        name: 'Jane Smith',
+        date: '2023-07-10',
+        rating: 4.5,
+        review: 'Very comfortable and well-located.',
+        durationOfStay: 'Feb 2023 - Apr 2023',
+      },
+      {
+        id: 3,
+        name: 'Alice Johnson',
+        date: '2023-06-05',
+        rating: 3.3,
+        review: 'Average experience, could be better.',
+        durationOfStay: 'Mar 2023 - May 2023',
+      }
     ]
   };
+
+  // Navigate to write a Review Page
+  const handleWriteReview = () => {
+    router.push(`/apartment/${apartmentId}/rate-apartment`);
+  }
 
   return (
     <ScreenWrapper
@@ -43,7 +83,7 @@ export default function RatingsPage() {
           Overall Rating
         </Text>
 
-        <Text className='text-secondary text-8xl font-dmserif mt-5 leading-tight'>
+        <Text className='text-secondary text-9xl font-dmserif mt-5 leading-tight'>
           {ratings.overallRating}
         </Text>
 
@@ -86,8 +126,46 @@ export default function RatingsPage() {
       </View>
 
       {/* List of Tenants Reviews */}
-      <View>
-        
+      
+      {/* Title */}
+      <View className='mt-10 flex-row items-center justify-between'>
+        <Text className='text-text text-base font-poppinsMedium'>
+          Tenant Reviews
+        </Text>
+
+        <DropdownButton 
+          bottomSheetLabel={'Sort Reviews By'} 
+          options={['Most Recent', 'Highest Rating', 'Lowest Rating']}
+          value={selectedFilter}
+          onSelect={(value) => setSelectedFilter(value)}        
+        />
+      </View>
+
+      <Divider />
+
+      {/* Render List of Tenant Reviews */}
+      <View className='flex gap-5'>
+        {
+          ratings.reviews.map(({ id, name, date, rating, review, durationOfStay }) => (
+            <RatingCard 
+              key={id}
+              name={name}
+              date={date}
+              rating={rating}
+              review={review}
+              durationOfStay={durationOfStay}
+            />
+          ))
+        }
+      </View>
+
+      {/* Review Button */}
+      <View className='mt-10'>
+        <PillButton 
+          label='Write a Review'          
+          onPress={handleWriteReview}
+          leftIconName={IconCirclePlus}
+        />
       </View>
     </ScreenWrapper>
   )
--- apps/mobile/components/display/Divider.tsx
+++ apps/mobile/components/display/Divider.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native'
 export default function Divider() {
   return (
     <View
-      className='w-full h-[2px] rounded-full bg-grey-100 my-5'
+      className={`w-full h-[2px] rounded-full bg-grey-100 my-5`}
     />
   )
 }
