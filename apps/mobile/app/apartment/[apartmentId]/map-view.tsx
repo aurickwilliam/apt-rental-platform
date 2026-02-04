bf6246bc105b56636fb7a76e20a89801e116d62a
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-04 11:33:37 +0800

    ADDED: File Needed and Update the Layout File

---
+++ apps/mobile/app/apartment/[apartmentId]/map-view.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function MapView() {
+  return (
+    <View>
+      <Text>MapView</Text>
+    </View>
+  )
+}
---
+++ apps/mobile/app/apartment/[apartmentId]/view-lease.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function ViewLease() {
+  return (
+    <View>
+      <Text>ViewLease</Text>
+    </View>
+  )
+}
