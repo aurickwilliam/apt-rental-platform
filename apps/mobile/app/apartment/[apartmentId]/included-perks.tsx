2ffe8f171e618af63354636adcc0bfc344daf9b8
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-05 08:54:18 +0800

    DONE: Render Perks with ID

--- apps/mobile/app/apartment/[apartmentId]/included-perks.tsx
+++ apps/mobile/app/apartment/[apartmentId]/included-perks.tsx
@@ -1,11 +1,29 @@
 import { View, Text } from 'react-native'
+import { useLocalSearchParams } from 'expo-router'
 
 import ScreenWrapper from 'components/layout/ScreenWrapper'
 import StandardHeader from 'components/layout/StandardHeader'
+import PerkItem from 'components/display/PerkItem'
 
 import { COLORS } from '../../../constants/colors'
 
 export default function IncludedPerks() {
+  const { apartmentId } = useLocalSearchParams();
+
+  // Dummy Data for Included Perks
+  const includedPerks = [
+    'wifi',
+    'ac',
+    'tv',
+    'kitchen',
+    'parking',
+    'hotwater',
+    'bath'
+  ]
+
+  // TODO: Fetch the actual included perks for the apartment using the apartmentId from the params
+  // TODO: Sort the perks based on categories (e.g. Essentials, Appliances, etc.)
+
   return (
     <ScreenWrapper
       scrollable
@@ -13,8 +31,13 @@ export default function IncludedPerks() {
         <StandardHeader title='Included Perks' />
       }
       headerBackgroundColor={COLORS.primary}
+      className='p-5'
     >
-      <Text>IncludedPerks</Text>
+      <View className='flex items-start gap-3'>
+        {includedPerks.map(perkId => (
+          <PerkItem key={perkId} perkId={perkId} />
+        ))}
+      </View>
     </ScreenWrapper>
   )
 }
