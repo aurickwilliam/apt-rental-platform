975af497c90e064f3aca05f6ba4bff9b629270f3
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-08 20:50:40 +0800

    Implemented the Image Viewing in Apartment Screen

--- apps/mobile/app/apartment/[apartmentId]/view-lease.tsx
+++ apps/mobile/app/apartment/[apartmentId]/view-lease.tsx
@@ -1,9 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+
+import ScreenWrapper from 'components/layout/ScreenWrapper'
+import StandardHeader from 'components/layout/StandardHeader'
+
+import { SAMPLE_IMAGES } from 'constants/images'
 
 export default function ViewLease() {
   return (
-    <View>
-      <Text>ViewLease</Text>
-    </View>
+    <ScreenWrapper
+      header={
+        <StandardHeader title="Lease Agreement" />
+      }
+    >
+      <View className='flex-1 items-center justify-center'>
+        <Image 
+          source={SAMPLE_IMAGES.sampleLeaseAgreement}
+          style={{ width: '100%', height: '100%' }}
+          resizeMode='contain'
+        />
+      </View>
+    </ScreenWrapper>
   )
 }
--- apps/mobile/constants/images.ts
+++ apps/mobile/constants/images.ts
@@ -3,10 +3,6 @@ export const IMAGES = {
   googleLogo: require("../assets/images/third-party-logos/google.png"),
   facebookLogo: require("../assets/images/third-party-logos/facebook.png"),
   appleLogo: require("../assets/images/third-party-logos/apple.png"),
-  defaultProfilePicture: require("../assets/images/default-profile.jpg"),
-  defaultThumbnail: require("../assets/images/default-thumbnail.jpeg"),
-  sampleBackgroundPhoto: require("../assets/images/sample-bg-photo.png"),
-  sampleProfilePicture: require("../assets/images/sample-profile-pic.jpg"),
 };
 
 export const EMPTY_STATE_IMAGES = {
@@ -22,4 +18,15 @@ export const ONBOARDING_IMAGES = {
   onboardingImage2: require("../assets/images/splash-image/explore-rentals.png"),
   onboardingImage3: require("../assets/images/splash-image/secure-transactions.png"),
   onboardingImage4: require("../assets/images/splash-image/verified-listing.png"),
+}
+
+export const SAMPLE_IMAGES = {
+  sampleBackgroundPhoto: require("../assets/images/sample-bg-photo.png"),
+  sampleProfilePicture: require("../assets/images/sample-profile-pic.jpg"),
+  sampleLeaseAgreement: require("../assets/images/sample-lease-agreement.jpg"),
+}
+
+export const DEFAULT_IMAGES = {
+  defaultProfilePicture: require("../assets/images/default-profile.jpg"),
+  defaultThumbnail: require("../assets/images/default-thumbnail.jpeg"),
 }
