af70983ba97985008c142fc4c14c469d4b7e8a28
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-08 21:01:20 +0800

    DONE: View Lease Agreement in Apartment Screen

--- apps/mobile/app/apartment/[apartmentId]/view-lease.tsx
+++ apps/mobile/app/apartment/[apartmentId]/view-lease.tsx
@@ -1,4 +1,8 @@
-import { View, Text, Image } from 'react-native'
+import { Image, TouchableOpacity } from 'react-native'
+import { useState } from 'react';
+import { useLocalSearchParams } from 'expo-router';
+import ImageViewing from 'react-native-image-viewing';
+
 
 import ScreenWrapper from 'components/layout/ScreenWrapper'
 import StandardHeader from 'components/layout/StandardHeader'
@@ -6,19 +10,37 @@ import StandardHeader from 'components/layout/StandardHeader'
 import { SAMPLE_IMAGES } from 'constants/images'
 
 export default function ViewLease() {
+  const { apartmentId } = useLocalSearchParams<{ apartmentId: string }>();
+
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  
+  const leaseAgreementImage = SAMPLE_IMAGES.sampleLeaseAgreement || {uri: ''};
+
   return (
     <ScreenWrapper
       header={
         <StandardHeader title="Lease Agreement" />
       }
+      className='p-5 items-center justify-center'
     >
-      <View className='flex-1 items-center justify-center'>
+      <TouchableOpacity 
+        activeOpacity={0.7}
+        className='w-full h-[70%] border border-grey-300 rounded-2xl'
+        onPress={() => setIsVisible(true)}
+      >
         <Image 
-          source={SAMPLE_IMAGES.sampleLeaseAgreement}
+          source={leaseAgreementImage}
           style={{ width: '100%', height: '100%' }}
           resizeMode='contain'
         />
-      </View>
+      </TouchableOpacity>
+
+      <ImageViewing
+        images={[leaseAgreementImage]} 
+        imageIndex={0} 
+        visible={isVisible} 
+        onRequestClose={() => setIsVisible(false)}      
+      />
     </ScreenWrapper>
   )
 }
