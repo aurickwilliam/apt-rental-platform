ed73429865786a450c505f5080d4b1cda2a1ce26
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-09 21:57:25 +0800

    DONE: First Step Tenant Information

---
+++ apps/mobile/components/display/ApplicationHeader.tsx
@@ -0,0 +1,37 @@
+import { View, Text } from 'react-native'
+import CircleProgress from './CircleProgress'
+
+interface ApplicationHeaderProps {
+  currentTitle: string,
+  nextTitle: string,
+  step: number,
+  totalSteps?: number,
+}
+
+export default function ApplicationHeader({
+  currentTitle,
+  nextTitle,
+  step = 1,
+  totalSteps = 4,
+}: ApplicationHeaderProps) {
+
+  return (
+    <View className='w-full bg-white p-5 flex-row items-center justify-between'>
+      {/* Title and Next Title */}
+      <View className='flex gap-1'>
+        <Text className='text-2xl font-poppinsSemiBold text-text'>
+          {currentTitle}
+        </Text>
+
+        <Text className='text-base font-interMedium text-grey-500'>
+          Next: {nextTitle}
+        </Text>
+      </View>
+
+      {/* Circle Progress */}
+      <View>
+        <CircleProgress currentStep={step} totalSteps={totalSteps} />
+      </View>
+    </View>
+  )
+}
