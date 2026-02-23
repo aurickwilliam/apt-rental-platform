d5d19464ac3c2d7f092697bfb4374a538379c575
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-23 21:06:57 +0800

    DONE: Folder Structure

---
+++ apps/mobile/app/tenant/payment/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router"
+
+export default function _layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="index"/>
+      <Stack.Screen name="success"/>
+      <Stack.Screen name="methods"/>
+      <Stack.Screen name="failed"/>
+    </Stack>
+  )
+}
---
+++ apps/mobile/app/tenant/payment/failed.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Failed() {
+  return (
+    <View>
+      <Text>Failed</Text>
+    </View>
+  )
+}
