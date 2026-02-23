6229112d967d72ce5b77e9577a2a850cb3d55333
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-23 12:14:21 +0800

    ADDED: Maitenance Categories

---
+++ packages/constants/src/maintenance-categories.ts
@@ -0,0 +1,10 @@
+export const MAINTENANCE_CATEGORIES = [
+  'Plumbing & Water Fixtures',
+  'Appliances',
+  'HVAC (Climate Control)',
+  'Electrical & Lighting',
+  'General "Wear and Tear"',
+  'Pest Control',
+  'Structural Issues',
+  'Safety & Security',
+]
