3e3e42a6f2198b92c3e9f7f812c6c04695b6ef02
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-24 19:26:10 +0800

    DONE: List of Uploaded Documents

---
+++ apps/mobile/app/document-id/details.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+import ScreenWrapper from '@/components/layout/ScreenWrapper'
+
+export default function Details() {
+  return (
+    <ScreenWrapper>
+      <Text>Details</Text>
+    </ScreenWrapper>
+  )
+}
