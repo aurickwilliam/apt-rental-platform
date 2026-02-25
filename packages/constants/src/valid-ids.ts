ed445d000db08938d6276445985106236fed5336
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-26 00:38:13 +0800

    DONE: Select a Valid ID Page

---
+++ packages/constants/src/valid-ids.ts
@@ -0,0 +1,18 @@
+export const VALID_IDS = [
+  'Driver’s License',
+  'National ID (PhilSys/PhilID)',
+  'Passport',
+  'UMID Card',
+  'PRC ID',
+  'Social Security System (SSS) Card',
+  'Voter’s ID',
+  'Postal ID',
+  'Senior Citizen ID',
+  'PWD ID',
+]
+
+export const SECONDARY_IDS = [
+  'Company ID',
+  'School ID',
+  'TIN ID',
+]
