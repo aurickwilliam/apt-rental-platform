0b8b4e0074ec2520433de1182ca245a25db7dbce
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-27 22:28:07 +0800

    DONE: Grid Stats

--- apps/mobile/app/(tabs)/(landlord)/dashboard.tsx
+++ apps/mobile/app/(tabs)/(landlord)/dashboard.tsx
@@ -42,6 +42,33 @@ export default function Dashboard() {
           />
         </TouchableOpacity>
       </View>
+
+      {/* Grid Stats */}
+      <View className='flex gap-3'>
+        <View className='flex-row gap-3'>
+          <View className='flex-1 bg-primary rounded-2xl p-4 gap-1'>
+            <Text className='text-sm text-darkerWhite font-interMedium'>Total Properties</Text>
+            <Text className='text-4xl text-white font-interSemiBold'>5</Text>
+          </View>
+
+          <View className='flex-1 bg-white rounded-2xl p-4 gap-1 border border-grey-200'>
+            <Text className='text-sm text-gray-500 font-interMedium'>Total Properties</Text>
+            <Text className='text-4xl font-interSemiBold'>5</Text>
+          </View>
+        </View>
+
+        <View className='flex-row gap-3'>
+          <View className='flex-1 bg-white rounded-2xl p-4 gap-1 border border-grey-200'>
+            <Text className='text-sm text-gray-500 font-interMedium'>Total Properties</Text>
+            <Text className='text-4xl font-interSemiBold'>5</Text>
+          </View>
+
+          <View className='flex-1 bg-darkerWhite rounded-2xl p-4 gap-1'>
+            <Text className='text-sm text-gray-500 font-interMedium'>Total Properties</Text>
+            <Text className='text-4xl font-interSemiBold'>5</Text>
+          </View>
+        </View>
+      </View>
     </ScreenWrapper>
   )
 } 
