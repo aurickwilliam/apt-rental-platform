add7ef0406faeee763ae9262165a0b6e867b1d56
aurickwilliam <aurickwilliam21@gmail.com>
2026-02-27 18:49:41 +0800

    REFACTOR: Change the tab bar color

--- apps/mobile/app/(tabs)/(landlord)/dashboard.tsx
+++ apps/mobile/app/(tabs)/(landlord)/dashboard.tsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { useRouter } from 'expo-router'
+
+import { IMAGES } from '@/constants/images'
+import { COLORS } from '@repo/constants'
+
+import ScreenWrapper from '@/components/layout/ScreenWrapper'
+
+import {
+  IconBell
+} from "@tabler/icons-react-native"
 
 export default function Dashboard() {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
+    <ScreenWrapper
+      scrollable
+      className='p-5'
+      bottomPadding={50}
+    >
+      <View className='flex-row items-center justify-between mb-5'>
+        {/* Logo and Label */}
+        <View className='flex-row gap-3 items-center'>
+          <Image 
+            source={IMAGES.logo}
+            className='size-9'
+            resizeMode='contain'
+          />
+
+          <Text className='text-secondary text-4xl font-dmserif'>
+            Dashboard
+          </Text>
+        </View>
+
+        {/* Notification */}
+        <TouchableOpacity>
+          <IconBell
+            size={26}
+            color={COLORS.grey}
+            onPress={() => router.push('/landlord-notif')}
+          />
+        </TouchableOpacity>
+      </View>
+    </ScreenWrapper>
   )
-}
+} 
