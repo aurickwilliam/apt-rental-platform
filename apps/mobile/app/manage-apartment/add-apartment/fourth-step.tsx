37c866d6f316f0f14c86889bd9daa6d586bf3155
aurickwilliam <aurickwilliam21@gmail.com>
2026-03-03 20:58:59 +0800

    DONE: Set up the File Routing

---
+++ apps/mobile/app/manage-apartment/add-apartment/fourth-step.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function FourthStep() {
+  return (
+    <View>
+      <Text>FourthStep</Text>
+    </View>
+  )
+}
---
+++ apps/mobile/app/manage-apartment/add-apartment/map-pin.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function MapPin() {
+  return (
+    <View>
+      <Text>MapPin</Text>
+    </View>
+  )
+}
