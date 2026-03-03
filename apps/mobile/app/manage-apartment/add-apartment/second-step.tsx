bae9ea80a2112f1079297b0b7e0fd5306dddc973
aurickwilliam <aurickwilliam21@gmail.com>
2026-03-03 20:58:59 +0800

    DONE: Set up the File Routing

---
+++ apps/mobile/app/manage-apartment/add-apartment/second-step.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function SecondStep() {
+  return (
+    <View>
+      <Text>SecondStep</Text>
+    </View>
+  )
+}
---
+++ apps/mobile/app/manage-apartment/add-apartment/third-step.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function ThirdStep() {
+  return (
+    <View>
+      <Text>ThirdStep</Text>
+    </View>
+  )
+}
