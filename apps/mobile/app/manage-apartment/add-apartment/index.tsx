c1baf8195a2f54e09b8bdbae07878fd44f1ebb99
aurickwilliam <aurickwilliam21@gmail.com>
2026-03-03 21:08:40 +0800

    DONE: First Step

--- apps/mobile/app/manage-apartment/add-apartment/index.tsx
+++ apps/mobile/app/manage-apartment/add-apartment/index.tsx
@@ -1,9 +1,67 @@
 import { View, Text } from 'react-native'
+import { useRouter } from 'expo-router'
+
+import ScreenWrapper from '@/components/layout/ScreenWrapper'
+
+import { COLORS } from '@repo/constants'
+import ApplicationHeader from '@/components/display/ApplicationHeader'
+import UploadImageField from '@/components/inputs/UploadImageField'
+import TextField from '@/components/inputs/TextField'
+import PillButton from '@/components/buttons/PillButton'
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Index</Text>
-    </View>
+    <ScreenWrapper
+      scrollable
+      backgroundColor={COLORS.darkerWhite}
+    >
+      <ApplicationHeader 
+        currentTitle={'Photos & Title'} 
+        nextTitle={'Basic Info'} 
+        step={1}   
+        totalSteps={5}     
+      />
+
+      <View className='p-5 flex'>
+        <View className='flex gap-5'>
+          <TextField 
+            label='Apartment Name:'
+            required
+            placeholder='Enter apartment name'
+          />
+
+          <UploadImageField 
+            label='Add a Thumbnail:'
+            required
+          />
+
+          <UploadImageField 
+            label='Add Additional Photos:'
+            required
+          />
+        </View>
+
+        {/* Cancel or Next Button */}
+        <View className='flex-1 flex-row mt-16 gap-4'>
+          <View className='flex-1'>
+            <PillButton
+              label={'Cancel'}
+              type='danger'
+              isFullWidth
+              onPress={() => router.back()}
+            />
+          </View>
+          <View className='flex-1'>
+            <PillButton
+              label={'Next'}
+              isFullWidth
+              onPress={() => {}}
+            />
+          </View>
+        </View>
+      </View>
+    </ScreenWrapper>
   )
 }
