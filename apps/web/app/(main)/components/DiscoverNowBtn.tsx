c795e956418eb94a2a66b48fc359a33506658e26
aurickwilliam <aurickwilliam21@gmail.com>
2026-03-04 23:05:51 +0800

    DONE: How it Works

---
+++ apps/web/app/(main)/components/DiscoverNowBtn.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@heroui/react";
+
+export default function DiscoverNowBtn() {
+  return (
+    <Button
+      variant="bordered"
+      radius="full"
+    >
+      Discover Now
+    </Button>
+  );
+}
