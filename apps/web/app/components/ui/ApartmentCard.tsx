2e38cb07b706083aabe77d9aeb24ee67ba9c17a9
Aurick William Lorenzo <aurickwilliam21@gmail.com>
2026-05-16 23:11:54 +0800

    REFACTORED: Main Page refactored

--- apps/web/app/components/ui/ApartmentCard.tsx
+++ apps/web/app/components/ui/ApartmentCard.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import { Card, CardBody, CardFooter, Button } from "@heroui/react";
-
+import { Card, Button, Spinner } from "@heroui/react";
 import Image from "next/image";
-
-import {
-  Star,
-  Heart,
-} from "lucide-react"
-
+import { Star, Heart } from "lucide-react";
 import { formatCurrency } from "@repo/utils";
+import { div } from "framer-motion/client";
 
 interface ApartmentCardProps {
   name: string;
@@ -37,68 +32,69 @@ export default function ApartmentCard({
   const showFavoriteButton = Boolean(onFavoritePress);
 
   return (
-    <Card
-      className="bg-white rounded-xl shadow-sm overflow-hidden w-56 h-full relative"
-      isPressable
-      onPress={onPress}
+    <div
+      className="cursor-pointer"
+      onClick={onPress}
+      role="button"
     >
-      <CardBody className="p-0">
-        <Image
-          src={thumbnailUrl}
-          alt="Apartment Thumbnail"
-          width={300}
-          height={200}
-          className="rounded-xl object-cover size-56"
-        />
+      <Card className="bg-white rounded-xl shadow-sm overflow-hidden w-56 h-full relative">
+        <Card.Content className="p-0">
+          <Image
+            src={thumbnailUrl}
+            alt="Apartment Thumbnail"
+            width={300}
+            height={200}
+            className="rounded-xl object-cover size-56"
+          />
 
-        <div className="p-2">
-          <h3 className="text-base font-semibold truncate">
-            {name}
-          </h3>
+          <div className="p-2">
+            <h3 className="text-base font-semibold truncate">{name}</h3>
+
+            <div className="flex items-center gap-1">
+              <p className="text-gray-600 text-sm">{location}</p>
+            </div>
+          </div>
+        </Card.Content>
+
+        <Card.Footer className="p-2 flex justify-between items-center">
+          <p className="text-base font-medium text-primary">
+            ₱ {formatCurrency(price)}
+          </p>
 
           <div className="flex items-center gap-1">
-            <p className="text-gray-600 text-sm">
-              {location}
-            </p>
+            <Star className="text-yellow-400" fill="currentColor" size={18} />
+            <p className="text-sm">{rating === 0 ? "-" : rating}</p>
           </div>
-        </div>
-      </CardBody>
+        </Card.Footer>
 
-      <CardFooter className="p-2 flex justify-between items-center">
-        <p className="text-base font-medium text-primary">
-          ₱ {formatCurrency(price)}
-        </p>
-
-        <div className="flex items-center gap-1">
-          <Star
-            className="text-yellow-400"
-            fill="currentColor"
-            size={18}
-          />
-          <p className="text-sm">
-            {rating == 0 ? "-" : rating}
-          </p>
-        </div>
-      </CardFooter>
-
-      {showFavoriteButton ? (
-        <Button
-          as="div"
-          variant="flat"
-          radius="full"
-          className="absolute top-2 right-2 bg-black/30"
-          isIconOnly
-          isLoading={isFavoriteLoading}
-          onPress={() => {
-            onFavoritePress?.();
-          }}
-        >
-          <Heart
-            className={isFavorite ? "text-red-500" : "text-white"}
-            fill={isFavorite ? "currentColor" : "none"}
-          />
-        </Button>
-      ) : null}
-    </Card>
+        {showFavoriteButton ? (
+          <Button
+            variant="ghost"
+            className="absolute top-2 right-2 bg-black/30"
+            isIconOnly
+            isPending={isFavoriteLoading}
+            onPress={(e) => {
+              onFavoritePress?.();
+            }}
+          >
+            {({ isPending }) => (
+              <>
+                {isPending ? (
+                  <Spinner 
+                    color="current" 
+                    size="sm" 
+                  />
+                ) : (
+                  <Heart
+                    className={isFavorite ? "text-red-500" : "text-white"}
+                    fill={isFavorite ? "currentColor" : "none"}
+                  />
+                )}
+              </>
+            )}
+          </Button>
+        ) : null}
+      </Card>
+    </div>
   );
 }
